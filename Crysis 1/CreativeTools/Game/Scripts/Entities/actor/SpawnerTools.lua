5b34f402b2b7ab2050a840b3530e6d4da11938c9
EnergyNick <nsikachin@gmail.com>
2025-03-09 03:29:12 +0500

    Add ability to spawn crew team, add more options

--- Crysis 1/CreativeTools/Game/Scripts/Entities/actor/SpawnerTools.lua
+++ Crysis 1/CreativeTools/Game/Scripts/Entities/actor/SpawnerTools.lua
@@ -1,12 +1,15 @@
-Script.ReloadScript( "SCRIPTS/SpawnEntityList.lua");
+Script.ReloadScript("SCRIPTS/SpawnEntityList.lua");
+Script.ReloadScript("SCRIPTS/HUD/Hud.lua");
 
 
 DebugGunProperties = {
 	minDistance = 3,
+	maxDistance = 25,
 	currentElementIndex = 1,
 	currentCategoryIndex = 1,
 	totalCategories = count(DebugGunSpawnList),
-	spawnedEntityPool = {}
+	spawnedEntityPool = {},
+	creationIndexSequence = 0
 }
 
 local function getElementInCategory(index)
@@ -58,11 +61,42 @@ local function tryFindElementAndSetByName(elementName)
 	return false
 end
 
--- Customization of entity
-function CustomizeSpawned(entity, spawnInfo)
+local function getNewIndexForSpawnedEntity()
+	local res = DebugGunProperties.creationIndexSequence
+	DebugGunProperties.creationIndexSequence = res + 1
+	return res
+end
 
-	if (entity and spawnInfo.name == "vtol") then
+
+function Player:EntityAdditionalActions(entity, spawnInfo, newEntitiesGroup)
+
+	if (spawnInfo.name == "vtol") then
 		entity.vehicle:SetAmmoCount("a2ahomingmissile", 12);
+	end
+
+	if spawnInfo.crew then
+		for i, currentItem in pairs(spawnInfo.crew) do
+			local position = entity:GetPos();
+			position.x = position.x + 5
+
+			local name = currentItem.name.." ["..getNewIndexForSpawnedEntity().."]"
+			local params = {
+				class = currentItem.class,
+				archetype = currentItem.archetype,
+				position = position,
+				orientation = entity:GetDirectionVector(1),
+				name = name,
+				scale = self:GetScale(),
+				properties = currentItem.properties,
+				-- propertiesInstance = self.PropertiesInstance,
+			}
+			local subEntity = System.SpawnEntity(params)
+
+			if(subEntity) then
+				EnterVehicle(subEntity, entity, i, true)
+				table.insert(newEntitiesGroup, subEntity)
+			end
+		end
 	end
 end
 
@@ -75,52 +109,54 @@ function Player:SpawnerToolAction(action, isPressed, isHold)
 
     if (action == "attack1" and isPressed) then
 
-		local wep = self.inventory:GetCurrentItem();
-		if (wep and wep.class == "DebugGun") then
+		local currentItem = getCurrentElement();
 
-			local currentItem = getCurrentElement();
+		local rayfilter = ent_all;
+		local rayLimit = currentItem.maxDistanceOverride or DebugGunProperties.maxDistance;
 
-			local rayfilter = ent_all;
-			local rayLimit = 25;
+		local direction = g_Vectors.temp_v1;
+		CopyVector(direction, System.GetViewCameraDir());
+		ScaleVectorInPlace(direction, rayLimit);
 
-			local direction = g_Vectors.temp_v1;
-			local position = g_Vectors.temp_v2;
-			self.actor:GetHeadPos(position);
-			CopyVector(direction, System.GetViewCameraDir());
-			ScaleVectorInPlace(direction, rayLimit);
+		local position = g_Vectors.temp_v2;
+		self.actor:GetHeadPos(position);
 
-			local hits = Physics.RayWorldIntersection(position,direction,2,rayfilter,self.id,nil,g_HitTable);
+		local hits = Physics.RayWorldIntersection(position,direction,2,rayfilter,self.id,nil,g_HitTable);
 
-			if (hits == 0 or g_HitTable[1].dist < DebugGunProperties.minDistance) then
-				HUD.HitIndicator();
-				return;
-			end
+		local minDistanceToSpawn = currentItem.minDistanceOverride or DebugGunProperties.minDistance
+		if (hits == 0 or g_HitTable[1].dist < minDistanceToSpawn) then
+			HUD.HitIndicator();
+			return;
+		end
 
-			local pos = g_HitTable[1].pos
-			pos.z = pos.z + currentItem.zOffset
+		local pos = g_HitTable[1].pos
+		pos.z = pos.z + currentItem.zOffset
 
-			local params = {
-				class = currentItem.class,
-				archetype = currentItem.archetype,
-				position = pos,
-				orientation = self:GetDirectionVector(1),
-				name = currentItem.name,
-				scale = self:GetScale(),
-				-- properties = self.Properties,
-				-- propertiesInstance = self.PropertiesInstance,
+		local spawnParam = currentItem.properties
+		local params = {
+			class = currentItem.class,
+			archetype = currentItem.archetype,
+			position = pos,
+			orientation = self:GetDirectionVector(1),
+			name = currentItem.name.." ["..getNewIndexForSpawnedEntity().."]",
+			scale = self:GetScale(),
+			properties = spawnParam,
+			-- propertiesInstance = self.PropertiesInstance,
+		}
+		local entity = System.SpawnEntity(params)
+
+		if (entity) then
+			local newEntitiesGroup = {
+				["0"] = entity
 			}
-			local entity = System.SpawnEntity(params)
+			self:EntityAdditionalActions(entity, currentItem, newEntitiesGroup)
 
-			if (entity) then
-				HUD.DrawStatusText("Spawned ["..currentItem.name.."]");
+			table.insert(DebugGunProperties.spawnedEntityPool, newEntitiesGroup)
 
-				CustomizeSpawned(entity, currentItem)
-
-				table.insert(DebugGunProperties.spawnedEntityPool, entity)
-			else
-				-- If you see that error enter to console "log_verbosity 3" to see error message
-				HUD.DisplayBigOverlayFlashMessage("Error: entity not spawned", 4, 400, 275, { x=1, y=0, z=0});
-			end
+			HUD.DrawStatusText("Spawned ["..currentItem.name.."]");
+		else
+			-- If you see that error enter to console "log_verbosity 3" to see error message
+			HUD.DisplayBigOverlayFlashMessage("Error: entity not spawned", 4, 400, 275, { x=1, y=0, z=0});
 		end
 	end
 
@@ -132,7 +168,6 @@ function Player:SpawnerToolAction(action, isPressed, isHold)
 
 		HUD.DisplayBigOverlayFlashMessage("Switch category to ["..newCategoryName.."]", 2, 400, 375, { x=0.3, y=1, z=0.3 });
 	end
-
 
 	if (action == "zoom" and isPressed) then
 
@@ -176,10 +211,12 @@ function Player:SpawnerToolAction(action, isPressed, isHold)
 			return;
 		end
 
-		local lastEntity = DebugGunProperties.spawnedEntityPool[lastIndex]
+		local lastEntityGroup = DebugGunProperties.spawnedEntityPool[lastIndex]
 		table.remove(DebugGunProperties.spawnedEntityPool, lastIndex)
-		System.RemoveEntity(lastEntity.id);
+		for key, value in pairs(lastEntityGroup) do
+			System.RemoveEntity(value.id);
+		end
 
-		HUD.DrawStatusText("Removed ["..lastEntity:GetName().."]");
+		HUD.DrawStatusText("Removed ["..lastEntityGroup:GetName().."]");
 	end
 end
