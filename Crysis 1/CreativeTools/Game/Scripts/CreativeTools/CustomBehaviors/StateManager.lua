469a0ac629536088e2d8931ddcab04f86e185529
EnergyNick <nsikachin@gmail.com>
2025-03-15 01:22:04 +0500

    Extend logic and rename state manager

--- Crysis 1/CreativeTools/Game/Scripts/CreativeTools/CustomBehaviors/StateManager.lua
+++ Crysis 1/CreativeTools/Game/Scripts/CreativeTools/CustomBehaviors/StateManager.lua
@@ -2,9 +2,6 @@ Script.ReloadScript("SCRIPTS/HUD/Hud.lua");
 Script.ReloadScript("Scripts/Utils/Math.lua");
 Script.ReloadScript("Scripts/CreativeTools/CustomBehaviors/StateMachine.lua");
 
-local function defaultOnStateChange(self, event, from, to)
-    self.timePointOfOperation = 0
-end
 
 function CreateStateManagerBasedOnPreset(preset, entity)
     local machineProvider = GetFiniteStateMachine()
@@ -14,25 +11,47 @@ function CreateStateManagerBasedOnPreset(preset, entity)
         callbacks = preset.fsmCallbacks
     })
 
-    fsm.Actions = preset.fsmStateActions
-    fsm.onstatechange = preset.defaultOnStateChange or defaultOnStateChange
-    fsm.timePointOfOperation = 0
-
     fsm.entity = entity
+    fsm.Actions = preset.fsmStateActions
+
+    fsm.timePointOfOperation = 0
+    fsm.onstatechange = function (self, event, from, to)
+        self.timePointOfOperation = 0
+        self.onceOperationExecuted = false
+
+        if (preset.defaultOnStateChange) then
+            preset.defaultOnStateChange(self, event, from, to)
+        end
+    end
+
+    fsm.enableBehavior = true
+    fsm.CompleteBehavior = function (self, isDead)
+        self.enableBehavior = false
+        if (preset.onCompleteAction) then
+            preset.onCompleteAction(self, isDead)
+        end
+    end
+
     return fsm
 end
 
 
 local function safetyIterationHelper(state)
-  if not state.nextIterationTimer then
+  if state.enableBehavior and not state.nextIterationTimer then
     state.nextIterationTimer = Script.SetTimer(1000, StateManagerIteration, state)
   end
 end
 
+
 function StateManagerIteration(state)
     state.nextIterationTimer = nil
 
-    if (state.entity:IsDead()) then return end
+    if (not state.enableBehavior) then return end
+
+    if (state.entity:IsDead()) then
+        state:CompleteBehavior(true)
+        return
+    end
 
     local action = state.Actions[state.current]
     if (not action) then
@@ -44,4 +63,8 @@ function StateManagerIteration(state)
     local resultTime = action(state)
 
     state.nextIterationTimer = Script.SetTimer(resultTime or 1000, StateManagerIteration, state)
+end
+
+function RunStateManagerAsync(state)
+    Script.SetTimer(1000, StateManagerIteration, state)
 end
